2ce19830c2d2d524893d6eda3e47e7c990ab2c5f
John Murphy <faw987@gmail.com>
2013-03-10 14:33:57 -0400

    Second Commit Step

---
+++ AndroidGoogleApiJavaClient/src/com/ecs/android/foursquare/oauth2/OAuth2ClientCredentials.java
@@ -0,0 +1,16 @@
+package com.ecs.android.foursquare.oauth2;
+
+public class OAuth2ClientCredentials {
+
+	public static final String CLIENT_ID = "K4FFGWINCKUXMSDRT1DFGUP02NKOTZAHIHCKCH100QSOUBYL";
+
+	public static final String CLIENT_SECRET = "VG1STWGNPZSS0ZAHPCOOSAYMOLWQ3UDARR0BFPFOKFW3NU1Q";
+
+	public static final String AUTHORIZATION_URL = "https://foursquare.com/oauth2/authenticate";
+	
+	public static final String ACCESS_TOKEN_URL = "https://foursquare.com/oauth2/access_token";
+	
+	public static final String REDIRECT_URI = "https://www.p1.com/callback1";
+
+
+}
